5089fcf33040d9bd3ed84e0ec6de3fbb93a0bcfe
david <david>
2020-04-11 20:55:10 +0200

    add logic to register a new user

--- src/app/modules/auth/page/register/register.component.ts
+++ src/app/modules/auth/page/register/register.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ValidationService} from '../../../../shared/service/validation.service';
+import {Router} from "@angular/router";
+import {catchError, tap} from "rxjs/operators";
+import {of} from "rxjs";
+import {AuthService} from "../../../../core/service/auth.service";
 
 @Component({
   selector: 'app-register',
@@ -6,4 +12,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  error: string;
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder,
+              private authService: AuthService,
+              private router: Router) {
+    this.buildForm();
+  }
+
+  get form () {
+    return this.registerForm.controls;
+  }
+
+  buildForm() {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password:  ['', [ Validators.required, ValidationService.passwordValidator]],
+      email:  ['', [ Validators.required, ValidationService.emailValidator]],
+    });
+  }
+
+  registerUser() {
+    const user = this.registerForm.value;
+    this.authService.register(user).pipe(
+      tap(user => this.router.navigate(['/auth/login'])),
+      catchError(error => of(this.error = error))
+    ).subscribe();
+  }
+
 }
