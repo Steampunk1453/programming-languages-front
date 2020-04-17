7a9d3707c8e7ded93641adc0c2583613ba576436
david <david>
2020-04-17 18:43:34 +0200

    add to avoid user navigate without auth

--- src/app/core/guard/auth.guard.ts
+++ src/app/core/guard/auth.guard.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {AuthService} from "../service/auth.service";
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class AuthGuard implements CanActivate {
-  canActivate(): boolean {
-    return true;
+
+  constructor(private authService: AuthService,
+              private router: Router) {}
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    if (!this.authService.isAuthenticated) {
+      this.router.navigate(['/auth/login'])
+      return false;
+    } else {
+      return true;
+    }
   }
+
 }
