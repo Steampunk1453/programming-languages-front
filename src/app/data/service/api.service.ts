5e19c8999aa29a2f0f758ee6fe35c97aea8fd287
david <david>
2020-04-17 19:02:19 +0200

    add new CRUD methods

--- src/app/data/service/api.service.ts
+++ src/app/data/service/api.service.ts
@@ -1,19 +1,34 @@
 import {Injectable} from '@angular/core';
 import {Observable} from "rxjs";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private REST_API_SERVER = "http://localhost:8080";
 
   constructor(private http: HttpClient) {
   }
 
   post(endPoint: string, body: Object = {}): Observable<any> {
-    return this.http.post(`${this.REST_API_SERVER}${endPoint}`, body)
-      .pipe();
+    return this.http.post(`${environment.API_URL}${endPoint}`, body).pipe();
+  }
+
+  getAll(endPoint: string): Observable<any> {
+    return this.http.get(`${environment.API_URL}${endPoint}`).pipe();
+  }
+
+  get(endPoint: string): Observable<any> {
+    return this.http.get(`${environment.API_URL}${endPoint}`).pipe();
+  }
+
+  put(endPoint: string, body: Object = {}): Observable<any> {
+    return this.http.put(`${environment.API_URL}${endPoint}`, body).pipe();
+  }
+
+  delete(endPoint: string): Observable<any> {
+    return this.http.delete(`${environment.API_URL}${endPoint}`).pipe();
   }
 
 }
