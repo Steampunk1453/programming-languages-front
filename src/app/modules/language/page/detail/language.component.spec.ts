88772453d5b7cd232ef38f86beac9fa0ca46467a
david <david>
2020-04-19 19:00:07 +0200

    update with CRUD operations logic

--- src/app/modules/language/page/detail/language.component.spec.ts
+++ src/app/modules/language/page/detail/language.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { LanguageComponent } from './language.component';
+import {LanguageComponent} from './language.component';
 
 describe('LanguageComponent', () => {
   let component: LanguageComponent;
