3ee1929b4f6e4c5386b81b0405fce57d7b37cfb4
david <david>
2020-04-19 19:07:52 +0200

    add logout action

--- src/app/layout/nav/nav.component.ts
+++ src/app/layout/nav/nav.component.ts
@@ -1,23 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
-import { Observable } from 'rxjs';
-import {environment} from "../../../environments/environment";
+import {Component} from '@angular/core';
+import {AuthService} from "../../core/service/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit {
-
+export class NavComponent {
   navItems = [
     { link: '/home', title: 'Home' },
-    { link: '/languages', title: 'Languages' }
+    { link: '/languages/detail', title: 'Create Language' },
   ];
 
-  constructor() {}
+  constructor(private authService: AuthService,
+              private router: Router) {}
 
-  ngOnInit() {
+  logout() {
+    this.authService.logout()
+    this.router.navigate(['/auth/login'])
   }
-
 }
