0504f68ac262d3e5085e001f6c59a8ff5316b0da
david <david>
2020-04-19 19:05:51 +0200

    remove getAll, it's possible use get for all read requests

--- src/app/data/service/api.service.ts
+++ src/app/data/service/api.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Observable} from "rxjs";
-import {HttpClient, HttpParams} from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
 
 @Injectable({
@@ -13,10 +13,6 @@ export class ApiService {
 
   post(endPoint: string, body: Object = {}): Observable<any> {
     return this.http.post(`${environment.API_URL}${endPoint}`, body).pipe();
-  }
-
-  getAll(endPoint: string): Observable<any> {
-    return this.http.get(`${environment.API_URL}${endPoint}`).pipe();
   }
 
   get(endPoint: string): Observable<any> {
