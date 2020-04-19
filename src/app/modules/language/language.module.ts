12f3f66794db83188eec48e1ea621e7baf2ed0c3
david <david>
2020-04-19 19:09:42 +0200

    add material elements

--- src/app/modules/language/language.module.ts
+++ src/app/modules/language/language.module.ts
@@ -1,16 +1,24 @@
 import {NgModule} from '@angular/core';
-import {LanguageComponent} from "./page/language.component";
+import {LanguageComponent} from "./page/detail/language.component";
 import {LanguageRoutes} from "./language-routing.module";
 import {SharedModule} from "../../shared/shared.module";
 import {MatRadioModule} from "@angular/material/radio";
+import {LanguageListComponent} from './page/list/language-list.component';
+import {MatTableModule} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator";
 
 
 @NgModule({
-  declarations: [LanguageComponent],
+    declarations: [LanguageComponent, LanguageListComponent],
+    exports: [
+        LanguageListComponent
+    ],
     imports: [
         SharedModule,
         LanguageRoutes,
-        MatRadioModule
+        MatRadioModule,
+        MatTableModule,
+        MatPaginatorModule
     ]
 })
 export class LanguageModule { }
